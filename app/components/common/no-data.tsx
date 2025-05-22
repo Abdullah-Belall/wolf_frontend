db760fc03b0f206f7b0cd9fcebd90fc34d00e20c
WOLFYANOO <mahmoudkhoder155@gmail.com>
2025-05-22 23:54:21 +0300

    asd

--- app/components/common/no-data.tsx
+++ app/components/common/no-data.tsx
@@ -1,3 +1,7 @@
 export default function NoData() {
-  return <div className="mx-auto w-fit bg-myHover px-mainxxl py-mainxl mt-3">لا يوجد بيانات</div>;
+  return (
+    <div className="mx-auto w-fit bg-myHover px-mainxxl py-mainxl mt-3 rounded-md">
+      لا يوجد بيانات
+    </div>
+  );
 }
